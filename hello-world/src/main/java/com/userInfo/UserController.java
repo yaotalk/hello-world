d14ff97a97ebf9fad593e3a04af95124ad277bfa
yaotalk <2362198826@qq.com>
2016-12-29 14:10:23 +0800

    # idea 测试

---
+++ hello-world/src/main/java/com/userInfo/UserController.java
@@ -0,0 +1,18 @@
+package com.userInfo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yaotalk on 2016/12/29.
+ */
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping("getUserInfo")
+    public String getUserInfo(){
+        return "yao yao";
+    }
+
+}
